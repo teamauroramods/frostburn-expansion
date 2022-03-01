2b2ee5ce671145149f36ac4057f27c7b7517edcc
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-03-01 17:45:44 -0500

    Update everything & backpedal a bit

--- fabric/src/main/java/com/teamaurora/frostburn_expansion/core/fabric/FrostburnExpansionFabric.java
+++ fabric/src/main/java/com/teamaurora/frostburn_expansion/core/fabric/FrostburnExpansionFabric.java
@@ -3,7 +3,7 @@ package com.teamaurora.frostburn_expansion.core.fabric;
 import com.teamaurora.frostburn_expansion.core.FrostburnExpansion;
 import net.fabricmc.api.ModInitializer;
 
-public class StarterFabric implements ModInitializer {
+public class FrostburnExpansionFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         FrostburnExpansion.PLATFORM.setup();
--- fabric/src/main/java/com/teamaurora/frostburn_expansion/core/fabric/FrostburnExpansionFabricDatagen.java
+++ fabric/src/main/java/com/teamaurora/frostburn_expansion/core/fabric/FrostburnExpansionFabricDatagen.java
@@ -4,7 +4,7 @@ import com.teamaurora.frostburn_expansion.core.FrostburnExpansion;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class StarterFabricDatagen implements DataGeneratorEntrypoint {
+public class FrostburnExpansionFabricDatagen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
         FrostburnExpansion.PLATFORM.dataSetup(dataGenerator);
