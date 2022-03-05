ae372a9cfd53d5cbf0920ae4623153676f648622
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-03-05 13:53:07 -0500

    Borealene

--- common/src/main/java/com/teamaurora/frostburn_expansion/core/registry/FBExBlocks.java
+++ common/src/main/java/com/teamaurora/frostburn_expansion/core/registry/FBExBlocks.java
@@ -9,10 +9,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.RotatedPillarBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
@@ -27,10 +24,27 @@ public class FBExBlocks {
     public static final Supplier<Block> DIRTY_BASALT = registerBlock("dirty_basalt", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.BASALT)), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
     public static final Supplier<Block> OVERGROWN_BASALT = registerBlock("overgrown_basalt", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.BASALT)), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
 
+    public static final Supplier<Block> BOREALENE = registerBlock("borealene", () -> new Block(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_SLAB = registerBlock("borealene_slab", () -> new SlabBlock(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_STAIRS = registerBlock("borealene_stairs", () -> new StairBlock(FBExBlocks.BOREALENE.get().defaultBlockState(), Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_WALL = registerBlock("borealene_wall", () -> new WallBlock(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_BRICKS = registerBlock("borealene_bricks", () -> new Block(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_BRICK_SLAB = registerBlock("borealene_brick_slab", () -> new SlabBlock(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_BRICK_STAIRS = registerBlock("borealene_brick_stairs", () -> new StairBlock(FBExBlocks.BOREALENE.get().defaultBlockState(), Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_BRICK_WALL = registerBlock("borealene_brick_wall", () -> new WallBlock(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> CRACKED_BOREALENE_BRICKS = registerBlock("cracked_borealene_bricks", () -> new Block(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> CHISELED_BOREALENE_BRICKS = registerBlock("chiseled_borealene_bricks", () -> new Block(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_PILLAR = registerBlock("borealene_pillar", () -> new RotatedPillarBlock(Properties.BOREALENE), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
+    public static final Supplier<Block> BOREALENE_LAMP = registerBlock("borealene_lamp", () -> new Block(Properties.BOREALENE_LAMP), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
 
     private static Supplier<Block> registerBlock(String id, Supplier<Block> block, Item.Properties properties) {
         Supplier<Block> register = BLOCKS.register(id, block);
         FBExItems.ITEMS.register(id, () -> new BlockItem(register.get(), properties));
         return register;
+    }
+
+    public static final class Properties {
+        public static final BlockBehaviour.Properties BOREALENE = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_MAGENTA).strength(1.5f, 6.0f).requiresCorrectToolForDrops();
+        public static final BlockBehaviour.Properties BOREALENE_LAMP = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_MAGENTA).strength(1.5f, 6.0f).requiresCorrectToolForDrops().lightLevel(s -> 14);
     }
 }
