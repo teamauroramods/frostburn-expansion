a83750cc2e0dca5886a7fd76df8be0818aaeb876
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-04-17 14:33:30 -0400

    Add swelling for brisk

--- common/src/main/java/com/teamaurora/frostburn_expansion/client/render/entity/living/BriskRenderer.java
+++ common/src/main/java/com/teamaurora/frostburn_expansion/client/render/entity/living/BriskRenderer.java
@@ -1,5 +1,6 @@
 package com.teamaurora.frostburn_expansion.client.render.entity.living;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.teamaurora.frostburn_expansion.common.entity.Brisk;
 import com.teamaurora.frostburn_expansion.core.FrostburnExpansion;
 import gg.moonflower.pollen.pinwheel.api.client.animation.AnimatedEntityRenderer;
@@ -7,6 +8,8 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.monster.Creeper;
 
 
 @Environment(EnvType.CLIENT)
@@ -15,6 +18,22 @@ public class BriskRenderer extends AnimatedEntityRenderer<Brisk> {
 
     public BriskRenderer(EntityRendererProvider.Context context) {
         super(context, FrostburnExpansion.generateResourceLocation("brisk"), 1.0F);
+    }
+
+    protected void scale(Brisk brisk, PoseStack poseStack, float f) {
+        float g = brisk.getSwelling(f);
+        float h = 1.0F + Mth.sin(g * 100.0F) * g * 0.01F;
+        g = Mth.clamp(g, 0.0F, 1.0F);
+        g *= g;
+        g *= g;
+        float i = (1.0F + g * 0.4F) * h;
+        float j = (1.0F + g * 0.1F) / h;
+        poseStack.scale(i, j, i);
+    }
+
+    protected float getWhiteOverlayProgress(Brisk brisk, float f) {
+        float g = brisk.getSwelling(f);
+        return (int)(g * 10.0F) % 2 == 0 ? 0.0F : Mth.clamp(g, 0.5F, 1.0F);
     }
 
     @Override
