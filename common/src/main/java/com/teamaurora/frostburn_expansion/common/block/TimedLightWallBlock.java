cba7f0a0c48dcf9d4a7bcacb3f9ad43963c95aa3
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-04-24 07:48:19 -0400

    FINALLY make timed light walls work

--- common/src/main/java/com/teamaurora/frostburn_expansion/common/block/TimedLightWallBlock.java
+++ common/src/main/java/com/teamaurora/frostburn_expansion/common/block/TimedLightWallBlock.java
@@ -1,5 +1,6 @@
 package com.teamaurora.frostburn_expansion.common.block;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
@@ -10,21 +11,25 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.WallBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.WallSide;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
+import java.util.Map;
 import java.util.Random;
 import java.util.function.Function;
 
 public class TimedLightWallBlock extends WallBlock implements ITimedLightBlockBase {
-
     public Function<Level, Boolean> lightSupplier;
 
     public TimedLightWallBlock(Properties properties, Function<Level, Boolean> lightSupplier) {
         super(properties);
         this.lightSupplier = lightSupplier;
         this.registerDefaultState(this.stateDefinition.any().setValue(UP, true).setValue(NORTH_WALL, WallSide.NONE).setValue(EAST_WALL, WallSide.NONE).setValue(SOUTH_WALL, WallSide.NONE).setValue(WEST_WALL, WallSide.NONE).setValue(WATERLOGGED, false).setValue(LIT, false));
+
     }
 
     @Override
@@ -33,8 +38,50 @@ public class TimedLightWallBlock extends WallBlock implements ITimedLightBlockBa
     }
 
     @Override
+    public Map<BlockState, VoxelShape> makeShapes(float f, float g, float h, float i, float j, float k) {
+        float l = 8.0f - f;
+        float m = 8.0f + f;
+        float n = 8.0f - g;
+        float o = 8.0f + g;
+        VoxelShape voxelShape = Block.box(l, 0.0, l, m, h, m);
+        VoxelShape voxelShape2 = Block.box(n, i, 0.0, o, j, o);
+        VoxelShape voxelShape3 = Block.box(n, i, n, o, j, 16.0);
+        VoxelShape voxelShape4 = Block.box(0.0, i, n, o, j, o);
+        VoxelShape voxelShape5 = Block.box(n, i, n, 16.0, j, o);
+        VoxelShape voxelShape6 = Block.box(n, i, 0.0, o, k, o);
+        VoxelShape voxelShape7 = Block.box(n, i, n, o, k, 16.0);
+        VoxelShape voxelShape8 = Block.box(0.0, i, n, o, k, o);
+        VoxelShape voxelShape9 = Block.box(n, i, n, 16.0, k, o);
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = ImmutableMap.builder();
+        for (Boolean boolean_ : UP.getPossibleValues()) {
+            for (WallSide wallSide : EAST_WALL.getPossibleValues()) {
+                for (WallSide wallSide2 : NORTH_WALL.getPossibleValues()) {
+                    for (WallSide wallSide3 : WEST_WALL.getPossibleValues()) {
+                        for (WallSide wallSide4 : SOUTH_WALL.getPossibleValues()) {
+                            VoxelShape voxelShape10 = Shapes.empty();
+                            voxelShape10 = WallBlock.applyWallShape(voxelShape10, wallSide, voxelShape5, voxelShape9);
+                            voxelShape10 = WallBlock.applyWallShape(voxelShape10, wallSide3, voxelShape4, voxelShape8);
+                            voxelShape10 = WallBlock.applyWallShape(voxelShape10, wallSide2, voxelShape2, voxelShape6);
+                            voxelShape10 = WallBlock.applyWallShape(voxelShape10, wallSide4, voxelShape3, voxelShape7);
+                            if (boolean_.booleanValue()) {
+                                voxelShape10 = Shapes.or(voxelShape10, voxelShape);
+                            }
+                            BlockState blockState = (BlockState)((BlockState)((BlockState)((BlockState)((BlockState)this.defaultBlockState().setValue(UP, boolean_)).setValue(EAST_WALL, wallSide)).setValue(WEST_WALL, wallSide3)).setValue(NORTH_WALL, wallSide2)).setValue(SOUTH_WALL, wallSide4);
+                            builder.put((BlockState)blockState.setValue(WATERLOGGED, false).setValue(LIT, false), voxelShape10);
+                            builder.put((BlockState)blockState.setValue(WATERLOGGED, true).setValue(LIT, true), voxelShape10);
+                            builder.put((BlockState)blockState.setValue(WATERLOGGED, true).setValue(LIT, false), voxelShape10);
+                            builder.put((BlockState)blockState.setValue(WATERLOGGED, false).setValue(LIT, true), voxelShape10);
+                        }
+                    }
+                }
+            }
+        }
+        return builder.build();
+    }
+
+    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        LevelReader levelReader = context.getLevel();
+        Level levelReader = context.getLevel();
         BlockPos blockPos = context.getClickedPos();
         FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
         BlockPos blockPos2 = blockPos.north();
@@ -51,8 +98,8 @@ public class TimedLightWallBlock extends WallBlock implements ITimedLightBlockBa
         boolean bl2 = this.connectsTo(blockState2, blockState2.isFaceSturdy(levelReader, blockPos3, Direction.WEST), Direction.WEST);
         boolean bl3 = this.connectsTo(blockState3, blockState3.isFaceSturdy(levelReader, blockPos4, Direction.NORTH), Direction.NORTH);
         boolean bl4 = this.connectsTo(blockState4, blockState4.isFaceSturdy(levelReader, blockPos5, Direction.EAST), Direction.EAST);
-        BlockState blockState6 = this.defaultBlockState().setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
-        return this.updateShape(levelReader, blockState6, blockPos6, blockState5, bl, bl2, bl3, bl4).setValue(LIT, lightSupplier.apply(context.getLevel()));
+        BlockState blockState6 = (BlockState)this.defaultBlockState().setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
+        return this.updateShape(levelReader, blockState6, blockPos6, blockState5, bl, bl2, bl3, bl4).setValue(LIT, lightSupplier.apply(levelReader));
     }
 
     @Override
